9ed71b237cf4329676298da42f51f2c697e21a5f
derwentx <derwentx@gmail.com>
2020-03-28 22:15:12 +1100

    🐛 make build-server not rm everything

--- build-server.sh
+++ build-server.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 build () {
-  echo "Installing dependencies..."
+  name="$(basename "$PWD")"
+  echo "Installing dependencies for ${name}..."
   rm -rdf node_modules
   yarn
-  cat pi-spi-build-patch.js > node_modules/pi-spi/index.js
-  echo "Building..."
+  cp 'pi-spi-build-patch.js' 'node_modules/pi-spi/index.js'
+
+  echo "Building ${name} ..."
   npm run build
-  echo "Cleaning up..."
-  cd node_modules && sudo find . -maxdepth 1  -type d -not -name 'pi-spi' -not -name '.' -not -name '..' -maxdepth 1 -exec rm -rf {} + && cd ..
-  sudo find . -maxdepth 1  -type d -not -name 'node_modules' -not -name 'built' -not -name '.' -exec rm -rdf {} +
-  sudo find . -maxdepth 1  -type f -not -name 'start.sh' -exec rm -rdf {} +
+  echo "Cleaning up  for ${name} ..."
+  build_dir="../${name}-build/"
+  mkdir -p "${build_dir}/node_modules"
+  cp -r 'node_modules/pi-spi' "${build_dir}/node_modules"
+  cp 'start.sh' "$build_dir"
+  cd '../' || return
+    sudo rm -rdf "${name}.bak" \
+    && mv "$name" "${name}.bak" \
+    && mv "${name}-build" "$name"
+  cd "$name" || return
   ls
 }
 
